26c9efd35e82919cc8cc41fa174a521bc911d351
Rituraj <riturajkalita154@gmail.com>
2025-10-23 11:30:37 +0530

    vercel changes added

--- backend/server.js
+++ backend/server.js
@@ -1,51 +1,49 @@
 import express from "express";
 import dotenv from "dotenv";
-import databaseconnection from "./config/db.js";
+import databaseconnection from "./config/db.js";  
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import memberRoutes from "./routes/memberRoutes.js";
 import { errorHandler } from "./middleware/errorHandler.js";
-import  { ensureAdmins } from "./utlis/ensureAdmins.js";
-import messageRoutes from "./routes/messageRoutes.js"
-
-
+import { ensureAdmins } from "./utlis/ensureAdmins.js";
+import messageRoutes from "./routes/messageRoutes.js";
+import serverless from "serverless-http"; 
 
 dotenv.config({ path: ".env" });
+
 
 databaseconnection();
 ensureAdmins();
 
 const app = express();
 
-// Middlewares
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:8000", 
+    origin: [
+      "http://localhost:8000",            
+      "https://antarikhyajec-pied.vercel.app/"  
+    ],
     credentials: true,
   })
 );
 
 // Routes
-app.use("/api/auth", authRoutes);      
-app.use("/api/member", memberRoutes); 
+app.use("/api/auth", authRoutes);
+app.use("/api/member", memberRoutes);
 app.use("/api/message", messageRoutes);
-
 
 // Default route
 app.get("/", (req, res) => {
   res.send("Antarikhya API is running...");
 });
 
-// middleware for errors
+
 app.use(errorHandler);
 
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export const handler = serverless(app);
